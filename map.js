92ae95935fddec9e0c78e479121e4659b7050206
jennyg0 <jen2348@hotmail.com>
2020-09-17 19:20:32 +0000

    removed console.log statements

--- map.js
+++ map.js
@@ -20,9 +20,6 @@ const assertArraysEqual = function(array1, array2) {
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-    console.log('item BEFORE: ', item);
-    console.log('item AFTER: ', callback(item));
-    console.log('---');
     results.push(callback(item));
   }
   return results;
