cd9f9eec762b8b1e95eb3c6c85671fff087e8f5d
jennyg0 <jen2348@hotmail.com>
2020-09-17 19:44:05 +0000

    function to output array to a certain point given by callback fcn

---
+++ takeUntil.js
@@ -0,0 +1,14 @@
+const takeUntil = function(array, callback) {
+  //run through array until it reaches the callback
+  const output = [];
+  for (let item of array) {
+    //if callback not true, push item to output array
+    if (!callback(item)) {
+      output.push(item);
+    //if callback false, break out of the loop
+    } else {
+      break;
+    }
+  } 
+  return output
+};
