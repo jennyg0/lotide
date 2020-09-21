32a7107f6bcb008aa09c49036788d8c17e49edbc
jennyg0 <jen2348@hotmail.com>
2020-09-21 16:45:39 +0000

    modularized code to separate files for testing

--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arrays) {
   return arrays[0];
 };
 
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
