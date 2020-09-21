c412f23b6a7c3ea5504c4e60a95f22977f55b74e
jennyg0 <jen2348@hotmail.com>
2020-09-21 16:56:17 +0000

    refactoring using modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let isEqual = true;
   //check to see if the arrays are the same length
@@ -20,11 +12,5 @@ const eqArrays = function(array1, array2) {
   } return isEqual;
 };
 
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+module.exports = eqArrays;
 
