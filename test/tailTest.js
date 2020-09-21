dcd5ad17546d6beb977fdbffde333e6db24b731c
jennyg0 <jen2348@hotmail.com>
2020-09-21 18:06:38 +0000

    refactored using mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,22 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+  it('returns 2 for tail(["Hello", "Lighthouse", "Labs"]).length', () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+
+  it('returns "Lighthouse" for tail(["Hello", "Lighthouse", "Labs"])[0]', () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], 'Lighthouse');
+  });
+
+  it('returns "Labs" for tail(["Hello", "Lighthouse", "Labs"])[1]', () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], 'Labs');
+  });
+ 
+});
+
